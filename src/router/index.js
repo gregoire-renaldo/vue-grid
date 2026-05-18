a38d6167a777c70fdfaa1567d125bd6177fab083
gregoire renaldo <gregoire.renaldo@gmail.com>
2026-05-18 11:30:47 +0200

    Enhance playlist detail view with Spotify player integration and improved UI

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,6 @@ const router = createRouter({
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/callback', name: 'Callback', component: HomeView },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
     { path: '/playlists', name: 'Playlists', component: Playlists },
     {
       path: '/playlists/:id',
